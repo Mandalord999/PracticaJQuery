e06ca306f7adfbd44be7bb839207ea6907800b41
Angel <angel.ramirezg@hotmail.com>
2021-01-21 17:45:10 -0300

    coomit_jquery_v7

--- efectos.js
+++ efectos.js
@@ -1,3 +1,8 @@
-$(document).ready(function(){
-    
-})
+// $(document).ready(function(){
+    function efecto(){
+        // $(selector).efecto(velocidad, callback);
+        $('.caja').hide(3000, function(){
+            alert("oculto");
+        });
+    }
+// })
